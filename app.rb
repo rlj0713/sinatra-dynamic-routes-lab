54a1b533f46e1b7285430058e05eda93616d8d92
Reid <rlj0713@gmail.com>
2021-03-27 19:17:10 -0400

    commit tree 907e6a20487af893c07346e2f53e95333720a6eb

--- app.rb
+++ app.rb
@@ -1,6 +1,43 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+
+  get "/reversename/:name" do
+    @name = params[:name]
+    "#{@name.reverse}"
+  end
+
+  get "/square/:number" do
+    "#{params[:number].to_i * params[:number].to_i}"
+  end
+
+  get "/say/:number/:phrase" do
+    i = 0
+    string = []
+    while i < params[:number].to_i
+      string << "#{params[:phrase]}"
+      i += 1
+    end
+    string.join(' ')
+  end
+
+  get "/say/:word1/:word2/:word3/:word4/:word5" do
+    "#{params.values.join(' ')}."
+  end
+
+  get "/:operation/:number1/:number2" do
+    op = params[:operation]
+    n1 = params[:number1].to_i
+    n2 = params[:number2].to_i
+    if op == "add"
+      result = (n1 + n2).to_s
+    elsif op == "subtract"
+      result = (n1 - n2).to_s
+    elsif op == "multiply"
+      result = (n1 * n2).to_s
+    else
+      result = (n1 / n2).to_s
+    end
+  end
 
 end
